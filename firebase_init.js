4e61862fdf6b7a9a591d644e36fd6a01d7b89806
deepak <trojanhorsedc@gmail.com>
2021-10-26 21:37:27 +0530

    Private key fix

--- firebase_init.js
+++ firebase_init.js
@@ -7,12 +7,13 @@ const firebase_config = {
   "private_key_id": process.env.PRIVATE_KEY_ID,
   "private_key": process.env.PRIVATE_KEY,
   "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
+    "client_id": process.env.CLIENT_ID.replace(/\\n/g, '\n'),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 }
+console.log(firebase_config);
 
 firebase.initializeApp({
     credential: firebase.credential.cert(firebase_config)
